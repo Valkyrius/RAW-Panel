af5e851672efb81087b999ef68e4a2d51a6843ab
Tjeerd Ritsma <Tjeerd@Valkyrius.com>
2011-07-16 13:44:54 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>RAW-Panel Index page</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        echo "Hello World!";
+        ?>
+    </body>
+</html>
